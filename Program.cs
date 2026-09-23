aa91066aad284e264623ff3c0171fbb9f8d73031
bestsucces93@gmail.com <bestsuccess93@gmail.com>
2026-09-23 11:03:36 +0100

    Initial SimpleCast project

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter receiver IP address: ");
+string address = Console.ReadLine();
+Console.WriteLine("You entered: " + address);
+if (address == "192.168.1.20")
+{
+    Console.WriteLine("Receiver found!");
+}
+else
+{Console.WriteLine("Receiver not found");}
